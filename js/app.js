de183e90acd7d8a6e4823655842b67190a07084c
romamaxime <roma.maxime1994@gmail.com>
2017-03-09 15:06:52 +0100

    ajout du jeu

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
-var solution = 3 * Math.random();
+var solution = Math.round(3 * Math.random());
 console.log(solution);
-var response = prompt("Ajouter un nombre compris entre 0 et 3");
-console.log(response);
+var choice = true;
 
-if(response == solution){
-	console.log("Bravo! Vous avez trouvé la solution!");
-} else if(response < solution){
-	console.log("La solution est plus grande.");
-} else {
-	console.log("La solution est plus petite.");
-}
+for(var i = 0; i <= 2; i += 1){
+	var response = prompt("Ajouter un nombre compris entre 0 et 3");
+	if (i == 2){
+		console.log("Vous avez perdu!");
+	} else {
+		if (response == solution){
+			console.log("Bravo! Vous avez trouve la solution!");
+			break;
+		} else if(response < solution){
+			console.log("La solution est plus grande.")
+		} else {
+			console.log("La solution est plus petite.")
+		}
+	};
+};
