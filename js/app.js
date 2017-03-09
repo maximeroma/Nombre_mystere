17025d0b05f1d6a20ca87eb77b769366f76c1d78
romamaxime <roma.maxime1994@gmail.com>
2017-03-09 14:09:24 +0100

    ajout de la comparaison des deux variables

--- js/app.js
+++ js/app.js
@@ -2,3 +2,11 @@ var solution = 3 * Math.random();
 console.log(solution);
 var response = prompt("Ajouter un nombre compris entre 0 et 3");
 console.log(response);
+
+if(response == solution){
+	console.log("Bravo! Vous avez trouvé la solution!");
+} else if(response < solution){
+	console.log("La solution est plus grande.");
+} else {
+	console.log("La solution est plus petite.");
+}
