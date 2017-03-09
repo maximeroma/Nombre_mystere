20f58352e1880b3edd7fca7af9d874ab46d3fb3c
romamaxime <roma.maxime1994@gmail.com>
2017-03-09 16:56:38 +0100

    correction final exercice

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@ var solution = Math.round(3 * Math.random());
 console.log(solution);
 var choice = true;
 
-for(var i = 0; i <= 2; i += 1){
+//for(var i = 0; i <= 2; i += 1){ /ne pas utiliser "+=" mais "++"
+for(var i = 0; i <= 2; i++){
 	var response = prompt("Ajouter un nombre compris entre 0 et 3");
-	if (i == 2){
+	if (i === 2){ // === /difference: "2=='2'(true)" et "2==='2'(false)"
 		console.log("Vous avez perdu!");
 	} else {
 		if (response == solution){
@@ -17,3 +18,4 @@ for(var i = 0; i <= 2; i += 1){
 		}
 	};
 };
+
